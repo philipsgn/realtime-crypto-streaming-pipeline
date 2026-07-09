3fe3acbefb4c43206016e39a9a468f08963c9007
philipsgn <tanphat260705@gmail.com>
2026-07-09 11:38:34 +0700

    feat: align Grafana dashboards with Gold business metrics

--- dbt_project/models/gold/gold_minute_volume.sql
+++ dbt_project/models/gold/gold_minute_volume.sql
@@ -8,7 +8,12 @@ SELECT
     SUM(total_volume) AS base_volume,
     SUM(vwap * total_volume) AS quote_volume_usdt,
     SUM(vwap * total_volume) / NULLIF(SUM(total_volume), 0) AS minute_vwap,
-    SUM(trade_count) AS trade_count
+    SUM(trade_count) AS trade_count,
+    MAX(price_open) AS price_open,
+    MAX(price_close) AS price_close,
+    SUM(buy_volume) AS buy_volume,
+    (MAX(price_close) - MAX(price_open))
+        / NULLIF(MAX(price_open), 0) * 100 AS price_change_pct
 FROM {{ ref('silver_trades') }}
 GROUP BY
     window_start,
